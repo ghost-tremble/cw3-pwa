35c301fe726b8a7e1b2a57ffd3c24815d4bc4507
ghost-tremble <=>
2024-02-17 15:02:54 +0000

    fixed git config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-const repoName = "cw3-pwa"
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:`/${{repoName}}`
+ 
 })
